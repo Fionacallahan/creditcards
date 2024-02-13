b93ed8ab043c8fea66f2820d3d9f9c0c54e1af9a
fiona.callahan <fiona.callahan@MU-C1-01.mullen.pvt.k12.co.us>
2024-02-13 09:26:03 -0700

    completed first few methods

--- Credit Card/src/cardChecker.java
+++ Credit Card/src/cardChecker.java
@@ -1,24 +1,30 @@
 import java.util.Scanner;
+import java.util.ArrayList; 
 
 public class cardChecker {
-	public static void main(String[] args) {
-		long cardNumber = welcome();
+	public static void main(String[] args) 
+	{
+		static long number;
+		welcome();
+		loadArray();
 	}
-	public static int welcome()
+	public static long welcome()
 	{
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("HELLO! Welcome to Credit Card number Validation! Please input a 16 digit credit card number (no spaces) and I will tell you if this number is a possible validated number!");
-		long number = userInput.nextLong();
+		number = userInput.nextLong();
 		return number;
 	}
-	public void loadArray(long cardNumber)
+	public static ArrayList <Long> loadArray()
 	{
+		ArrayList <Long> original = new ArrayList<Long>();
 		while (cardNumber > 0)
 		{
 			int temporary = number % 10;
-			
+			number = number/10;
+			original[original.length()] = temporary;
 		}
+		return original;
 	}
-	long [] rawNumber
 
 }
