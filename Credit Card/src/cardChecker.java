3fdea3cc286ca955aaa293664e7db2b5c0a228fb
Administrator <Administrator@R90PKYBQ>
2024-02-14 21:04:08 -0600

    figured out errors

--- Credit Card/src/cardChecker.java
+++ Credit Card/src/cardChecker.java
@@ -1,29 +1,33 @@
 import java.util.Scanner;
-import java.util.ArrayList; 
+//import java.io.IOException;
+//import java.util.ArrayList; 
 
 public class cardChecker {
+	static Scanner userLongInput = new Scanner(System.in);
+	static long number;
+
 	public static void main(String[] args) 
 	{
-		static long number;
 		welcome();
 		loadArray();
 	}
 	public static long welcome()
 	{
-		Scanner userInput = new Scanner(System.in);
 		System.out.println("HELLO! Welcome to Credit Card number Validation! Please input a 16 digit credit card number (no spaces) and I will tell you if this number is a possible validated number!");
-		number = userInput.nextLong();
+		number = userLongInput.nextLong();
 		return number;
 	}
-	public static ArrayList <Long> loadArray()
+	public static long [] loadArray()
 	{
-		ArrayList <Long> original = new ArrayList<Long>();
-		while (cardNumber > 0)
+		long [] original = new long[16];
+		while (number > 0)
 		{
-			int temporary = number % 10;
+			int sum = 1;
+			long temporary = number % 10;
 			number = number/10;
-			original[original.length()] = temporary;
+			original[original.length - sum] = temporary;
 		}
+		System.out.println(original);
 		return original;
 	}
 
